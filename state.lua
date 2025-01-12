ca49143dd242061e7e80693252d62d70c69386de
wozrer <wozrer@proton.me>
2025-01-12 12:11:48 +0300

    Translate

--- state.lua
+++ state.lua
@@ -4,12 +4,12 @@ local M = {}
 M.states = {}
 
 M.create_state = function(player, name)
-	M.states[name] = { owner = player }
+	M.states[name] = { dictator = player }
 end
 
 M.state_info = function(name)
 	local state = M.states[name]
-	return S("name: @1\nowner: @2", name, state.owner)
+	return S("name: @1", name) .. "\n" .. S("dictator: @1", state.dictator)
 end
 
 return M
